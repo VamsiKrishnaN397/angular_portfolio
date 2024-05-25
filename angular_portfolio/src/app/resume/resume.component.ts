e34a650fc8bd70347534d02c405598d81c88da27
VamsiKrishnaN397 <nvamsikrishna.neelam@gmail.com>
2024-05-25 02:13:07 -0400

    Honors added

--- angular_portfolio/src/app/resume/resume.component.ts
+++ angular_portfolio/src/app/resume/resume.component.ts
@@ -9,5 +9,10 @@ import { SharedModule } from '../shared/shared.module';
   styleUrl: './resume.component.css'
 })
 export class ResumeComponent {
-
+  public honor = [
+    {sno: 1, imgCaption: "Gold Medal Awarding", url: "./assets/awarding.jpg"},
+    {sno: 2, imgCaption: "Gold Medal", url: "./assets/goldmedal.jpg"},
+    {sno: 3, imgCaption: "Gold Medal with Certificate", url: "./assets/certificate.jpg"},
+    {sno: 4, imgCaption: "Honors Board", url: "./assets/honorboard.jpg"}
+  ]
 }
